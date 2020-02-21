a6a34ab888d44e351aa8c7ca08a732b86c8d7523
Haleem <Haleem.Assal@gmail.com>
2020-02-21 01:05:26 -0400

    Add header that defines messages

---
+++ network/payload.h
@@ -0,0 +1,11 @@
+#ifndef PAYLOAD_H_
+#define PAYLOAD_H_
+
+// TODO - need this?
+
+// client -> server (REQUEST)
+#define CLIENT_REQUEST_MESSAGE_SIZE 2
+#define WIDTH_INDEX 0
+#define HEIGHT_INDEX 1
+
+#endif
